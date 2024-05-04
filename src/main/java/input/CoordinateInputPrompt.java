4aa50cab8daa8307f57c71a651543dcc7db383aa
audrb96 <audrb96@naver.com>
2024-05-04 16:52:11 +0900

    feature : CoordinateInputPrompt.java 생성
    - InputPrompt 구현체

---
+++ src/main/java/input/CoordinateInputPrompt.java
@@ -0,0 +1,16 @@
+package input;
+
+import dto.CoordinateUserInput;
+import util.Console;
+
+public class CoordinateInputPrompt implements InputPrompt {
+    private static final String INPUT_COORDINATE_NAVIGATION_STRING = "> 좌표를 입력하세요.";
+
+    @Override
+    public CoordinateUserInput prompt() {
+        System.out.println(INPUT_COORDINATE_NAVIGATION_STRING);
+        String userInput = Console.readLine();
+
+        return CoordinateUserInputPatternMatcher.match(userInput);
+    }
+}
