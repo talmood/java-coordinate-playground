7072f25712015d4a3e5eba80911ccd52cd421099
audrb96 <audrb96@naver.com>
2024-05-05 21:13:19 +0900

    feature : 계산 시작 안내문구 위치변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,10 @@ import service.CoordinateCalculatorCreator;
 
 public class Application {
 
+    private static final String START_CALCULATE_NAVIGATION = "> 계산을 시작합니다. \n";
+
     public static void main(String[] args) {
+        System.out.println(START_CALCULATE_NAVIGATION);
         CoordinateCalculator coordinateCalculator = CoordinateCalculatorCreator.createRetryable();
         coordinateCalculator.calculate();
     }
--- src/main/java/input/ConsoleInputPrompt.java
+++ src/main/java/input/ConsoleInputPrompt.java
@@ -4,11 +4,8 @@ import input.dto.ConsoleInput;
 
 public class ConsoleInputPrompt implements InputPrompt {
 
-    private static final String START_CALCULATE_NAVIGATION = "> 계산을 시작합니다. \n";
-
     @Override
     public ConsoleInput prompt() {
-        System.out.println(START_CALCULATE_NAVIGATION);
         InputPrompt inputPrompt = InputPromptCreator.createExceptionHandlingCoordinate();
 
         return inputPrompt.prompt();
