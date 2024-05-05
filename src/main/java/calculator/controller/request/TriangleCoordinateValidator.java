85d493c918f29ee576dac38b321aaaa63f7a08d7
lgm1007 <flxl654@gmail.com>
2024-05-05 19:56:49 +0900

    modify: 사용 안하는 상수 제거

--- src/main/java/calculator/controller/request/TriangleCoordinateValidator.java
+++ src/main/java/calculator/controller/request/TriangleCoordinateValidator.java
@@ -2,7 +2,6 @@ package calculator.controller.request;
 
 public class TriangleCoordinateValidator extends CoordinateValidator {
 	private static final int COORDINATE_COUNT = 3;
-	private static final int SAME_XY_COORDINATE_COUNT = 2;
 
 	private static final String WRONG_INPUT_COORDINATE_COUNT = " 삼각형 좌표는 두 개를 입력해야 합니다.";
 
