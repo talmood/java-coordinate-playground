d7ce38a25dbb1da30d56fe83bfbe1fd09d49818b
haero77 <shineecard5@gmail.com>
2024-05-06 22:41:29 +0900

    feat: 좌표 모델 객체 구현

---
+++ src/main/java/model/Point.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Point {
+
+    private static final int MAX_VALUE = 24;
+    private static final int MIN_VALUE = 0;
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        validateValueRange(x, y);
+        this.x = x;
+        this.y = y;
+    }
+
+    private void validateValueRange(int x, int y) {
+        if (!meetsValueRange(x) || !meetsValueRange(y)) {
+            throw new IllegalArgumentException(
+                    "X, Y 좌표의 범위는 최소 %d, 최대 %d입니다.".formatted(MIN_VALUE, MAX_VALUE)
+            );
+        }
+    }
+
+    private boolean meetsValueRange(int value) {
+        return value >= MIN_VALUE && value <= MAX_VALUE;
+    }
+
+}
