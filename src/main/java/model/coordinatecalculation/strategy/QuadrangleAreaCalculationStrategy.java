c85bcdaddb27ea81e903f3e9c5e2134c23710d25
haero77 <shineecard5@gmail.com>
2024-05-08 03:02:14 +0900

    feat: 사각형 좌표 계산기 구현

---
+++ src/main/java/model/coordinatecalculation/strategy/QuadrangleAreaCalculationStrategy.java
@@ -0,0 +1,19 @@
+package model.coordinatecalculation.strategy;
+
+import model.CoordinateCalculationResult;
+import model.coordinatecalculation.polygon.Rectangle;
+
+public class QuadrangleAreaCalculationStrategy implements CoordinateCalculationStrategy {
+
+    private final Rectangle rectangle;
+
+    public QuadrangleAreaCalculationStrategy(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    @Override
+    public CoordinateCalculationResult calculate() {
+        return CoordinateCalculationResult.fromQuadrangleArea(this.rectangle.calculateArea());
+    }
+
+}
