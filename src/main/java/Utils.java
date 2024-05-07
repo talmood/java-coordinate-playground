0574f6afed9b810af9849ed712c7e5ac052e1a66
monsteralover <kmj99301@gmail.com>
2024-05-08 02:35:41 +0900

    refactor : calculateDistance() Double 반환하도록 변경

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,5 +1,5 @@
 public class Utils {
-    public static double calculateDistance(Point point1, Point point2) {
+    public static Double calculateDistance(Point point1, Point point2) {
         return Math.sqrt(Math.pow(point2.getPointX() - point1.getPointX(), 2)
                 + Math.pow(point2.getPointY() - point1.getPointY(), 2));
     }
