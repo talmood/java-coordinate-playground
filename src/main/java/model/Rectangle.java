d2bedb5244e10dee548856ae7b7e55b798e005f0
haero77 <shineecard5@gmail.com>
2024-05-08 02:40:48 +0900

    feat: 직사각형 도메인 모델 추가

---
+++ src/main/java/model/Rectangle.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.Arrays;
+
+import static utils.GeometryUtils.calculateDistance;
+
+public record Rectangle(
+        DistinguishedCoordinates coordinates
+) {
+
+    public Rectangle {
+        validateRectangle(coordinates);
+    }
+
+    private void validateRectangle(DistinguishedCoordinates coordinates) {
+        validateSize(coordinates);
+        if (!isRectangle(coordinates)) {
+            throw new IllegalArgumentException("직사각형이 아닙니다.");
+        }
+    }
+
+    // todo 벡터 내적을 이용해서 내각이 수직을 이루는지 확인
+    private boolean isRectangle(DistinguishedCoordinates coordinates) {
+        final Point first = coordinates.fetchPointByIndex(0);
+        final Point second = coordinates.fetchPointByIndex(1);
+        final Point third = coordinates.fetchPointByIndex(2);
+        final Point fourth = coordinates.fetchPointByIndex(3);
+
+        final double[] distances = new double[6];
+        distances[0] = calculateDistance(first, second);
+        distances[1] = calculateDistance(first, third);
+        distances[2] = calculateDistance(first, fourth);
+        distances[3] = calculateDistance(second, third);
+        distances[4] = calculateDistance(second, fourth);
+        distances[5] = calculateDistance(third, fourth);
+
+        Arrays.sort(distances); // 거리 오름차순 정렬
+
+        return (distances[0] == distances[1]) // 가장 짧은 변 2개의 길이가 같고,
+                && (distances[2] == distances[3]) // 나머지 변의 길이가 서로 같고
+                && (distances[4] == distances[5]); // 대각선 길이가 서로 같으면 직사각형
+    }
+
+    private void validateSize(DistinguishedCoordinates coordinates) {
+        if (!coordinates.matchesSize(4)) {
+            throw new IllegalArgumentException("직사각형이 아닙니다.");
+        }
+    }
+
+}
