a9c564df82aa3a9392b32370fffa1b185f39c0a5
monsteralover <kmj99301@gmail.com>
2024-05-08 01:39:49 +0900

    feat : Point 객체 추가하여 좌표를 관리

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,5 +1,18 @@
-public class Point {
-    public Point (String userInput) {
 
+public class Point {
+    private double pointX;
+    private double pointY;
+
+    public Point(double pointX, double pointY) {
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+
+    public double getPointX() {
+        return pointX;
+    }
+
+    public double getPointY() {
+        return pointY;
     }
 }
