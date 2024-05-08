fb36348b2a56f794799b64b7d61dc7a9e3722706
jjanggu <ssc6839@gmail.com>
2024-05-09 01:22:20 +0900

    feat: 좌표 생성 시 좌표 범위 유효성 검증

--- src/test/java/Point.java
+++ src/test/java/Point.java
@@ -9,6 +9,16 @@ public class Point {
 	}
 
 	public static Point of(final int x, final int y) {
+		validateCoordinateRange(x, y);
 		return new Point(x, y);
+	}
+
+	private static void validateCoordinateRange(final int x, final int y) {
+		if (x < 0 || x > 24) {
+			throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. X, Y 좌표의 범위는 최소 0, 최대 24입니다.");
+		}
+		if (y < 0 || y > 24) {
+			throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. X, Y 좌표의 범위는 최소 0, 최대 24입니다.");
+		}
 	}
 }
--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -12,9 +12,25 @@ public class PointTest {
 		assertDoesNotThrow(() -> Point.of(x, 12));
 	}
 
+	@ValueSource(ints = {-1, 25})
+	@ParameterizedTest
+	void X_좌표는_0보다_작거나_24보다_큰_수를_지정할_수_없다(final int x) {
+		assertThatThrownBy(() -> Point.of(x, 12))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] 잘못된 입력입니다. X, Y 좌표의 범위는 최소 0, 최대 24입니다.");
+	}
+
 	@ValueSource(ints = {0, 24})
 	@ParameterizedTest
 	void Y_좌표는_0부터_24까지만_지정할_수_있다(final int y) {
 		assertDoesNotThrow(() -> Point.of(12, y));
+	}
+
+	@ValueSource(ints = {-1, 25})
+	@ParameterizedTest
+	void Y_좌표는_0보다_작거나_24보다_큰_수를_지정할_수_없다(final int y) {
+		assertThatThrownBy(() -> Point.of(12, y))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] 잘못된 입력입니다. X, Y 좌표의 범위는 최소 0, 최대 24입니다.");
 	}
 }
