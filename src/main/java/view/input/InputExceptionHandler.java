e1bfb4c18f431a9b22949139da118144be0d2022
haero77 <shineecard5@gmail.com>
2024-05-09 01:51:07 +0900

    feat: 입력 예외 시 재입력 받도록 하능 기능

---
+++ src/main/java/view/input/InputExceptionHandler.java
@@ -0,0 +1,39 @@
+package view.input;
+
+import view.ConsoleWriter;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class InputExceptionHandler {
+
+    private final ConsoleWriter consoleWriter;
+
+    public InputExceptionHandler(ConsoleWriter consoleWriter) {
+        this.consoleWriter = consoleWriter;
+    }
+
+    public <T> T input(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            writeErrorMessage(e);
+            return input(supplier);
+        }
+    }
+
+    private void writeErrorMessage(IllegalArgumentException e) {
+        consoleWriter.writeLineWithPromptPrefix(toExceptionMessageFormat(e.getMessage()));
+    }
+
+    private String toExceptionMessageFormat(final String message) {
+        final String errorPrefix = "[ERROR] 잘못된 입력입니다.";
+
+        if (Objects.isNull(message)) {
+            return errorPrefix;
+        }
+
+        return errorPrefix + message;
+    }
+
+}
