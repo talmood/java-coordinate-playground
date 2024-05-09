799d1f00a16562d671e58870b6a5acb72225f076
lgm1007 <flxl654@gmail.com>
2024-05-09 20:22:15 +0900

    modify: 오탈자 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ import calculator.controller.CalculateSimulator;
 public class Application {
 
 	public static void main(String[] args) {
-		CalculateSimulator.runCalculator();;
+		CalculateSimulator.runCalculator();
 	}
 
 }
